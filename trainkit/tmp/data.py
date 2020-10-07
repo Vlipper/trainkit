d4259e54d5beb5efe92f30232cbb7d41bdce96f4
Vlipper <ign.legeza@gmail.com>
2020-10-07 15:31:17 +0300

    copy from junk repo and restructurize

---
+++ trainkit/tmp/data.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+from typing import Optional, Tuple, Union
+
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+
+
+class DatasetProperties:
+    _mean: Optional[np.ndarray]
+    _std: Optional[np.ndarray]
+
+    @staticmethod
+    def __str_converter(value: str, param_name: str) -> np.ndarray:
+        value_list = value.split(',')
+        if len(value_list) != 3:
+            raise ValueError('param "{}" must be csv string with 3 elements'.format(param_name))
+
+        value_array = np.array(value_list, dtype=np.float32).reshape((1, 1, 3))
+
+        return value_array
+
+    @property
+    def mean(self):
+        return self._mean
+
+    @mean.setter
+    def mean(self, value: str):
+        self._mean = self.__str_converter(value, 'mean')
+
+    @property
+    def std(self):
+        return self._std
+
+    @std.setter
+    def std(self, value: str):
+        self._std = self.__str_converter(value, 'std')
+
+
+class DatasetBaseMixin(DatasetProperties, Dataset):
+    def __init__(self, run_params: dict, hyper_params: dict, **kwargs):
+        super().__init__()
+
+        self.desc = self.__read_data_description(kwargs['description_csv_path'])
+        self.sampler = kwargs['sampler']
+
+        self.num_workers = run_params['num_workers']
+
+        self.batch_size = hyper_params['batch_size']
+        self.mean, self.std = hyper_params['mean'], hyper_params['std']
+
+    def __len__(self):
+        return len(self.desc)
+
+    def __getitem__(self, idx: Union[int, Tuple[int, ...]]):
+        raise NotImplementedError
+
+    @staticmethod
+    def __read_data_description(desc_path: Path) -> list:
+        desc = pd.read_csv(desc_path)
+        desc = desc.values
+
+        return desc
+
+    def _standartize(self, img: np.ndarray) -> np.ndarray:
+        img_mod = (img - self.mean) / self.std
+
+        return img_mod
+
+    @staticmethod
+    def _reorder_img_axes(img: np.ndarray, axes_order: Tuple[int, ...]) -> np.ndarray:
+        img_mod = np.transpose(img, axes_order)
+
+        return img_mod
+
+    def get_dataloader(self):
+        # shuffle = False if is_val else True
+        # shuffle=shuffle,
+        loader = DataLoader(dataset=self, batch_size=self.batch_size, sampler=self.sampler,
+                            num_workers=self.num_workers)
+
+        return loader
+
+
+# class BaseSet(DatasetBaseMixin):
+#     def __init__(self, run_params: dict, hyper_params: dict, **kwargs):
+#         super().__init__(run_params, hyper_params, **kwargs)
---
+++ trainkit/utils/trainer_utils.py
@@ -0,0 +1,77 @@
+from typing import Optional
+
+import torch.nn as nn
+from torch.optim import AdamW, SGD
+from torch.optim.lr_scheduler import CyclicLR, ReduceLROnPlateau
+from torch.optim.optimizer import Optimizer
+
+from trainkit.core.lr_scheduler import CstmOneCycleLR
+
+
+class OptimizerFactory:
+    @staticmethod
+    def get_optimizer(model: nn.Module, optimizer_name: str, optimizer_kwargs: dict):
+        if optimizer_name == 'sgd':
+            optimizer = SGD(model.parameters(), **optimizer_kwargs)
+        elif optimizer_name == 'adamw':
+            optimizer = AdamW(model.parameters(), **optimizer_kwargs)
+        else:
+            raise ValueError('Given optimizer_name: "{}" is not known'.format(optimizer_name))
+
+        return optimizer
+
+
+class LRSchedulerMixin:
+    num_epochs: Optional[int]
+    batches_per_epoch: Optional[int]
+
+    def get_scheduler(self, optimizer: Optimizer, lr_scheduler_name: str, lr_scheduler_kwargs: dict):
+        if lr_scheduler_name is None:
+            lr_scheduler = None
+        elif lr_scheduler_name == 'rop':
+            lr_scheduler = self.__get_rop_scheduler(optimizer, **lr_scheduler_kwargs)
+        elif lr_scheduler_name == 'clr':
+            lr_scheduler = self.__get_clr_scheduler(optimizer, **lr_scheduler_kwargs)
+        elif lr_scheduler_name == 'cst_onecycle':
+            lr_scheduler = self.__get_cst_onecycle_scheduler(optimizer, **lr_scheduler_kwargs)
+        else:
+            raise ValueError('Given lr_scheduler_name: "{}" is not known'.format(lr_scheduler_name))
+
+        return lr_scheduler
+
+    @staticmethod
+    def __get_rop_scheduler(optimizer, min_lr, max_lr, **kwargs):
+        # init new lr between min_lr and max_lr
+        rop_init_lr = min_lr + 0.5 * (max_lr - min_lr)
+        for group in optimizer.param_groups:
+            group['lr'] = rop_init_lr
+
+        lr_scheduler = ReduceLROnPlateau(optimizer, mode=kwargs['mode'], factor=kwargs['factor'],
+                                         threshold=kwargs['threshold'], patience=kwargs['patience'],
+                                         threshold_mode=kwargs['threshold_mode'],
+                                         cooldown=kwargs['cooldown'])
+        setattr(lr_scheduler, 'is_cyclic', False)
+
+        return lr_scheduler
+
+    def __get_clr_scheduler(self, optimizer, min_lr, max_lr, **kwargs):
+        clr_stepsize_up = int(kwargs['clr_step_size_up'] * self.batches_per_epoch)
+        clr_stepsize_down = int(kwargs['clr_step_size_down'] * self.batches_per_epoch)
+
+        lr_scheduler = CyclicLR(optimizer, base_lr=min_lr, max_lr=max_lr,
+                                step_size_up=clr_stepsize_up, step_size_down=clr_stepsize_down,
+                                mode=kwargs['mode'], cycle_momentum=kwargs['cycle_momentum'])
+        setattr(lr_scheduler, 'is_cyclic', True)
+
+        return lr_scheduler
+
+    def __get_cst_onecycle_scheduler(self, optimizer, max_lr, **kwargs):
+        lr_scheduler = CstmOneCycleLR(optimizer, max_lr=max_lr, epochs=self.num_epochs,
+                                      steps_per_epoch=self.batches_per_epoch,
+                                      step_frac_up=kwargs['step_frac_up'],
+                                      step_frac_down=kwargs['step_frac_down'],
+                                      init_lr_div_factor=kwargs['init_lr_div_factor'],
+                                      min_lr_div_factor=kwargs['min_lr_div_factor'])
+        setattr(lr_scheduler, 'is_cyclic', True)
+
+        return lr_scheduler
