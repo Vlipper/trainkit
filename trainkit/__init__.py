aca983f34ffe41f70202fd3f552cfd84a5f0d278
Vlipper <ign.legeza@gmail.com>
2021-03-12 18:36:26 +0300

    Remove old config parser and unused mod_params from HydraConfigModder

--- trainkit/__init__.py
+++ trainkit/__init__.py
@@ -1,4 +1,2 @@
 from trainkit.core.trainer import Trainer
 from trainkit.core.models import BaseNet
-
-from trainkit.utils.conf_parser import ConfParser
--- trainkit/utils/hydra_conf_parser.py
+++ trainkit/utils/hydra_conf_parser.py
@@ -19,26 +19,3 @@ class HydraConfigModder:
         self.hyper_params: dict = config['hyper_params']
 
         return self.run_params, self.hyper_params
-
-    # from torch import device as torch_device
-    # def _mod_run_params(self):
-    #     self._mod_param_device()
-    #
-    # def _mod_param_device(self):
-    #     device = self.run_params['general']['device']
-    #
-    #     self.run_params['general'].update({'device': torch_device(device)})
-
-        # general_params = self.run_params['general']
-        #
-        # device_split = general_params['device'].split(':')
-        # device_name = device_split[0]
-        #
-        # if device_name == 'cpu':
-        #     general_params.update({'device': torch_device('cpu')})
-        # elif device_name == 'cuda':
-        #     os.environ['CUDA_VISIBLE_DEVICES'] = device_split[1]
-        #     general_params.update({'device': torch_device('cuda')})
-        # else:
-        #     raise ValueError(f"Argument `device` must be one of: 'cpu', 'cuda:N', "
-        #                      f"got: '{general_params['device']}'")
