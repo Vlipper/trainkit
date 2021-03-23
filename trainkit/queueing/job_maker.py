eb11a3c419c27f8e6649c9d92e92b8193ef5de03
Vlipper <ign.legeza@gmail.com>
2021-03-23 23:50:58 +0300

    Add time kwargs into job queueing. Now timeout, result_ttl and failure_ttl are inf by default.

--- trainkit/queueing/job_maker.py
+++ trainkit/queueing/job_maker.py
@@ -1,8 +1,12 @@
 import shutil
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 from redis import Redis
 from rq import Queue
+
+if TYPE_CHECKING:
+    from typing import Optional
 
 
 class JobMaker:
@@ -15,18 +19,22 @@ class JobMaker:
                  db: str,
                  password: str,
                  queue_name: str,
-                 queue_timeout: int = 432000,  # 5 days (in seconds)
+                 job_timeout: 'Optional[str]' = None,
+                 ttl: 'Optional[str]' = None,
+                 result_ttl: 'Optional[str]' = None,
+                 failure_ttl: 'Optional[str]' = None,
                  func_path: str = 'setup_train.train',  # path to function (must be relative to (importable) src dir)
                  src_path: str = 'src',  # path to src dir (must be relative to it's project root)
                  cache_path: str = 'src_cache',  # path to src cache dir (must be relative to it's project root)
                  ):
-        self.run_params = run_params
-        self.hyper_params = hyper_params
+        # ToDo: add docstring
         self.proj_root_path = proj_root_path
-        self.src_path = Path(proj_root_path, src_path)
         self.func_path = func_path
+        self.src_path = Path(proj_root_path, src_path)
 
-        self.queue = self._get_queue(host, port, db, password, queue_name, queue_timeout)
+        self.func_kwargs = {'run_params': run_params, 'hyper_params': hyper_params}
+        self.enqueue_kwargs = self._parse_enqueue_kwargs(job_timeout, ttl, result_ttl, failure_ttl)
+        self.queue = self._get_queue(host, port, db, password, queue_name)
         self.cache_path = self._get_cache_path(proj_root_path, src_path, cache_path)
 
     def __call__(self):
@@ -35,14 +43,29 @@ class JobMaker:
         self._enqueue_job()
 
     @staticmethod
+    def _parse_enqueue_kwargs(job_timeout: 'Optional[str]' = None,
+                              ttl: 'Optional[str]' = None,
+                              result_ttl: 'Optional[str]' = None,
+                              failure_ttl: 'Optional[str]' = None) -> dict:
+        job_timeout = -1 if job_timeout is None else job_timeout
+        result_ttl = -1 if result_ttl is None else result_ttl
+        failure_ttl = -1 if failure_ttl is None else failure_ttl
+
+        enqueue_kwargs = {'job_timeout': job_timeout,
+                          'result_ttl': result_ttl,
+                          'failure_ttl': failure_ttl,
+                          'ttl': ttl}
+
+        return enqueue_kwargs
+
+    @staticmethod
     def _get_queue(host: str,
                    port: str,
                    db: str,
                    password: str,
-                   queue_name: str,
-                   queue_timeout: int) -> Queue:
+                   queue_name: str) -> Queue:
         connection = Redis(host=host, port=port, db=db, password=password)
-        queue = Queue(name=queue_name, default_timeout=queue_timeout, connection=connection)
+        queue = Queue(name=queue_name, connection=connection)
 
         return queue
 
@@ -66,6 +89,5 @@ class JobMaker:
         func_path = '.'.join(self.cache_path.parts[proj_root_idx:])
         func_path = f'{func_path}.{self.func_path}'
 
-        func_kwargs = {'run_params': self.run_params, 'hyper_params': self.hyper_params}
-        self.queue.enqueue(func_path, kwargs=func_kwargs)
+        self.queue.enqueue(func_path, kwargs=self.func_kwargs, **self.enqueue_kwargs)
 #         job_id=cache_path.parts[-2]
