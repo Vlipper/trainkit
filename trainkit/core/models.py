ec8c6b73fc6cf044963c0c257f76ed9a89d032bd
Vlipper <ign.legeza@gmail.com>
2021-06-30 15:23:22 +0300

    Add conditions if log_writer is None. Types and type fixes.

--- trainkit/core/models.py
+++ trainkit/core/models.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 import numpy as np
 from torch.nn import Module
 
 if TYPE_CHECKING:
+    from typing import Any, Dict
     from trainkit.core.trainer import Trainer
     import torch
 
@@ -16,7 +17,7 @@ class BaseOperationsMixin(ABC):
 
     @abstractmethod
     def batch_step(self, batch_idx: int,
-                   batch: Any) -> Dict[str, 'torch.Tensor']:
+                   batch: 'Any') -> 'Dict[str, torch.Tensor]':
         """
         Method receives batch (dataloaders' unmodified output on CPU) and its index.
         It must calc loss/metrics and save them into lists: batch_obj_losses, batch_obj_metrics.
@@ -33,6 +34,7 @@ class BaseOperationsMixin(ABC):
             >>> self.batch_obj_losses.extend(losses)
             >>> self.batch_obj_metrics.extend(metrics)
             >>> out = {'loss_backward': batch_loss}
+
             >>> return out
 
         Args:
@@ -57,7 +59,9 @@ class BaseOperationsMixin(ABC):
         pass
 
 
-class BaseNet(BaseOperationsMixin, Module, ABC):
+class BaseNet(BaseOperationsMixin,
+              Module,
+              ABC):
     trainer: 'Trainer'
     batch_obj_losses: list
     batch_obj_metrics: list
@@ -76,35 +80,39 @@ class BaseNet(BaseOperationsMixin, Module, ABC):
     def on_train_batch_end(self):
         # change lr if scheduler is cyclic
         if self.trainer.lr_scheduler is not None and self.trainer.lr_scheduler.is_cyclic:
-            self.trainer.log_writer.write_scalar('lr/train',
-                                                 self.trainer.optimizer.param_groups[0]['lr'],
-                                                 self.trainer.n_iter_train)
+            if self.trainer.log_writer is not None:
+                self.trainer.log_writer.write_scalar('lr/train',
+                                                     self.trainer.optimizer.param_groups[0]['lr'],
+                                                     self.trainer.n_iter_train)
             self.trainer.lr_scheduler.step()
 
     def on_train_part_end(self):
         # write mean values over batches into logs
-        self.trainer.log_writer.write_scalar('losses/train',
-                                             np.mean(self.batch_obj_losses).item(),
-                                             self.trainer.epoch)
-        self.trainer.log_writer.write_scalar('metrics/train',
-                                             np.mean(self.batch_obj_metrics).item(),
-                                             self.trainer.epoch)
+        if self.trainer.log_writer is not None:
+            self.trainer.log_writer.write_scalar('losses/train',
+                                                 np.mean(self.batch_obj_losses).item(),
+                                                 self.trainer.epoch)
+            self.trainer.log_writer.write_scalar('metrics/train',
+                                                 np.mean(self.batch_obj_metrics).item(),
+                                                 self.trainer.epoch)
 
     def on_val_part_end(self):
         # write mean values over batches into logs
         self.trainer.val_loss = np.mean(self.batch_obj_losses).item()
         self.trainer.val_metrics = np.mean(self.batch_obj_metrics).item()
 
-        self.trainer.log_writer.write_scalar('losses/val',
-                                             self.trainer.val_loss,
-                                             self.trainer.epoch)
-        self.trainer.log_writer.write_scalar('metrics/val',
-                                             self.trainer.val_metrics,
-                                             self.trainer.epoch)
+        if self.trainer.log_writer is not None:
+            self.trainer.log_writer.write_scalar('losses/val',
+                                                 self.trainer.val_loss,
+                                                 self.trainer.epoch)
+            self.trainer.log_writer.write_scalar('metrics/val',
+                                                 self.trainer.val_metrics,
+                                                 self.trainer.epoch)
 
         # change lr if scheduler is not cyclic
         if self.trainer.lr_scheduler is not None and (not self.trainer.lr_scheduler.is_cyclic):
-            self.trainer.log_writer.write_scalar('lr/val',
-                                                 self.trainer.optimizer.param_groups[0]['lr'],
-                                                 self.trainer.epoch)
+            if self.trainer.log_writer is not None:
+                self.trainer.log_writer.write_scalar('lr/val',
+                                                     self.trainer.optimizer.param_groups[0]['lr'],
+                                                     self.trainer.epoch)
             self.trainer.lr_scheduler.step(self.trainer.val_loss)
