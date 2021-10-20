923d7bbbde3fa06f193c92911585722b316d382b
Vlipper <ign.legeza@gmail.com>
2021-10-20 23:48:56 +0300

    refactor: Move find lr logic from Trainer and change LogWriter initialization logic
    LogWriter with tensorboard SummaryWriter initialized with Trainer initializaion. It means that logs will be saved by order.

--- trainkit/core/logger.py
+++ trainkit/core/logger.py
@@ -8,19 +8,13 @@ class LogWriter:
     def __init__(self, tboard_dir_path: Path,
                  hparam_dir_path: Path,
                  model_name: str,
-                 flush_secs=30):
+                 flush_secs=10):
         self.tb_writer = SummaryWriter(log_dir=str(Path(tboard_dir_path, model_name).resolve()),
                                        flush_secs=flush_secs)
         self.hparam_log_file_path = Path(hparam_dir_path, f'{model_name}.json')
 
         # self.cv_log_paths.append(logs_path)
 
-    def write_scalar(self, tag: str, value: float, step: int):
-        self.tb_writer.add_scalar(tag, value, step)
-
     def write_hparams(self, hparams: dict):
         with self.hparam_log_file_path.open('w') as file:
             json.dump(hparams, file, indent=4)
-
-    def close(self):
-        self.tb_writer.close()
