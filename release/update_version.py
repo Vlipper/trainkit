88adf176b1b8b090ba8fec38c1f62c44cb3e58fe
Vlipper <ign.legeza@gmail.com>
2022-03-15 16:44:04 +0000

    build: Change package version bumping [skip ci]
    Version bumps in two files: `./pyproject.toml` and `./trainkit/__init__.py`

--- release/update_version.py
+++ release/update_version.py
@@ -5,23 +5,25 @@ from pathlib import Path
 
 def parse_args() -> dict:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--init_file_path', type=Path, required=True)
+    parser.add_argument('-f', '--file_path', type=Path, required=True)
     parser.add_argument('-v', '--new_version', type=str, required=True)
     parsed_args = vars(parser.parse_args())
 
     return parsed_args
 
 
-def update_init_file(file_path: Path,
-                     new_version: str):
-    file_text = file_path.open().read()
-    file_text_mod = re.sub(r"__version__ = '.*'",
-                           f"__version__ = '{new_version}'",
-                           file_text)
+def update_file(file_path: Path, new_version: str):
+    file_text = file_path.read_text()
+    old_version = re.search(r'_{0,2}version_{0,2} = (\'|\")(.*)(\'|\")', file_text).group(2)
+
+    file_text_mod = file_text.replace(old_version, new_version, 1)
     file_path.write_text(file_text_mod)
 
 
-if __name__ == '__main__':
+def main():
     args = parse_args()
-    update_init_file(file_path=args['init_file_path'],
-                     new_version=args['new_version'])
+    update_file(file_path=args['file_path'], new_version=args['new_version'])
+
+
+if __name__ == '__main__':
+    main()
